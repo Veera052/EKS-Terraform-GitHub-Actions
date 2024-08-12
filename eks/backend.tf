9e6b7683412bc3fb7012be671e2cd2af8e603607
Veera052 <132237685+Veera052@users.noreply.github.com>
2024-08-12 20:40:55 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket"
+    bucket         = "my-aws-bucket10"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
